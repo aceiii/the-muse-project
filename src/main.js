b7d41f85b01d9c0bfcfbe4a9dfaaa647939b3413
Borin Ouch <borin.ouch@gmail.com>
2016-03-13 19:20:20 -0700

    Add reactify

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var $ = require("jquery");
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var App = require("./app.js");
+var App = require("./app.jsx");
 
 $(document).ready(function () {
    ReactDOM.render(React.createElement(App), document.getElementById("app")); 
